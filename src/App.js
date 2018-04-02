869a7d91e0d35a56999462f9a8fe8e3f0db0f5e4
Palash Mondal <palashm@live.in>
2018-04-02 20:47:13 +0530

    Passing Data with Props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Home from './components/Home';
 
 class App extends Component {
     render() {
+        // A sample car object that we will pass as props
+        let car = { type: 'Fiat', model: '500', color: 'White' };
+        let names = ['Noah', 'William', 'Ethan', 'James'];
         return (
             <div className="App">
                 <Header />
-                <Home />
+                <Home name={'Palash'} car={car} names={names}>
+                    <p>This is a paragraph!</p>
+                </Home>
             </div>
         );
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Home extends Component {
     render() {
+        console.log(this.props);
+        console.log(this.props.children);
+
+        let namesList = this.props.names.map((name, index) => <li key={index}>{name}</li>);
+
         return (
             <div>
                 <section id="content" className="body">
-                    Hello World!
+                    <p>Hello World!</p>
+                    <p>Your name is {this.props.name}</p>
+                    <p>Your car is {this.props.car.type + ' ' + this.props.car.model}</p>
+                    <p>Looping through an array:-</p>
+                    <ul>{namesList}</ul>
+                    <hr />
+                    {this.props.children}
                 </section>
 
                 <section className="body footer">&copy; PM {new Date().toLocaleString()}</section>
@@ -13,5 +25,12 @@ class Home extends Component {
         );
     }
 }
+
+Home.propTypes = {
+    name: PropTypes.string,
+    car: PropTypes.object,
+    names: PropTypes.array,
+    children: PropTypes.element.isRequired
+};
 
 export default Home;
