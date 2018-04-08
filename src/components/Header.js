4ae1fc6c6520cee7536ad96368a4410607286d17
Palash Mondal <palashm@live.in>
2018-04-08 20:43:46 +0530

    Stateless Functional Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,33 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-class Header extends Component {
-    render() {
-        return (
-            <header id="banner" className="body">
-                <h1>
-                    <a href="#">
-                        Learn React<br />
-                        <strong>Simplest case example of ReactJS</strong>
-                    </a>
-                </h1>
+const Header = props => (
+    <header id="banner" className="body">
+        <h1>
+            <a href="#">
+                Learn React<br />
+                <strong>Simplest case example of ReactJS</strong>
+            </a>
+        </h1>
 
-                <nav>
-                    <ul>
-                        <li className="active">
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Portfolio</a>
-                        </li>
+        <nav>
+            <ul>
+                <li className="active">
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Portfolio</a>
+                </li>
 
-                        <li>
-                            <a href="#">Blog</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        );
-    }
-}
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+);
 
 export default Header;
